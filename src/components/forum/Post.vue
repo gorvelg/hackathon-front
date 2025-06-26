c5402c411c2ebbb4aa46513bebddda644f96e482
Guillaume <gu.gorvel@distrame.fr>
2025-06-26 10:50:49 +0200

    style: add lion svg

--- src/components/forum/Post.vue
+++ src/components/forum/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="w-full max-w-3xl mx-auto bg-white border border-gray-200 rounded-2xl shadow p-8 space-y-6">
+  <article class="w-full max-w-3xl mx-auto bg-white border border-gray-200 rounded-2xl shadow p-8 space-y-6 z-99">
     <h1 class="text-3xl font-bold text-[#003787]">{{ title }}</h1>
     <p class="text-gray-600 text-sm">
       Publié par <span class="font-semibold">{{ author.name || 'Anonyme' }}</span>
